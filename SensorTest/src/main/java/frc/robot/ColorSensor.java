c8210ca170d2e8c59516b0e02d862dfde705d12f
Emma Tong <eat2004@gmail.com>
2019-12-03 15:57:02 -0500

    Initial commit

---
+++ SensorTest/src/main/java/frc/robot/ColorSensor.java
@@ -0,0 +1,205 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+import edu.wpi.first.wpilibj.I2C;
+import java.nio.ByteBuffer;
+
+//import java.nio.ByteOrder;
+
+/**
+ * https://docs.broadcom.com/docs/APDS-9151-DS
+ */
+public class ColorSensor {
+    private I2C sensor;    
+
+    /*
+    private final static int MAIN_CTRL=0x00;
+    private final static int PART_ID=0x06;
+    private final static int MAIN_STATUS=0x07;
+    private final static int INT_CFG=0x19;
+    private final static int INT_PST=0x1A;
+
+    /* Uses PS
+    private final static int PS_LED=0x01;
+    private final static int PS_PULSES=0x02;
+    private final static int PS_MEAS_RATE=0x03;
+    */
+    //Will use LS, doesn't use LSB/MSB (uses intervening data byte)
+    //Have to check which ones are needed
+    /*
+    private final static int LS_MEAS_RATE=0x04;
+    private final static int LS_GAIN=0x05;
+    private final static int LS_DATA_IR_1=0x0B;
+    private final static int LS_DATA_GREEN_1=0x0E;
+    private final static int LS_DATA_BLUE_1=0x11;
+    private final static int LS_DATA_RED_1=0x14;
+    private final static int LS_THRES_UP_1=0x22;
+    private final static int LS_THRES_LOW_1=0x25;
+    private final static int LS_THRES_VAR=0x27;
+    */
+
+    //L-low H-high
+    private final static int ENABLE_REGISTER=0x00;
+    private final static int ATIME_REGISTER=0x01;
+    private final static int WTIME_REGISTER=0x03;
+    private final static int AILTL_REGISTER=0x04;
+    private final static int AILTH_REGISTER=0x05;
+    private final static int AIHTL_REGISTER=0x06;
+    private final static int AIHTH_REGISTER=0x07;
+    private final static int PILTL_REGISTER=0x08;
+    private final static int PILTH_REGISTER=0x09;
+    private final static int PIHTL_REGISTER=0x0A;
+    private final static int PIHTH_REGISTER=0x0B;
+    private final static int PERS_REGISTER=0x0C;
+    private final static int CONFIG_REGISTER=0x0D;
+    private final static int PPULSE_REGISTER=0x0E;
+    private final static int CONTROL_REGISTER=0x0F;
+    private final static int REVISION_REGISTER=0x11;
+    private final static int ID_REGISTER=0x12;
+    private final static int STATUS_REGISTER=0x13;
+    private final static int CDATA_REGISTER=0x14;
+    private final static int CDATAH_REGISTER=0x15;
+    private final static int RDATA_REGISTER=0x16;
+    private final static int RDATAH_REGISTER=0x17;
+    private final static int GDATA_REGISTER=0x18;
+    private final static int GDATAH_REGISTER=0x19;
+    private final static int BDATA_REGISTER=0x1A;
+    private final static int BDATAH_REGISTER=0x1B;
+    private final static int PDATA_REGISTER=0x1C;
+    private final static int PDATAH_REGISTER=0x1D;
+
+    private final static int PON   = 0b00000001;
+    private final static int AEN   = 0b00000010;
+    private final static int PEN   = 0b00000100;
+    private final static int WEN   = 0b00001000;
+    private final static int AIEN  = 0b00010000;
+    private final static int PIEN  = 0b00100000;
+    
+    
+
+
+    public ColorSensor(I2C.Port port) {
+        sensor = new I2C(port, 0x39); //port, I2C address    
+
+        sensor.write(0x00, 0b00000011); //power on, color sensor on
+    }
+
+    protected int readWordRegister(int address) {
+  ByteBuffer buf = ByteBuffer.allocate(2);
+  sensor.read(COMMAND_REGISTER_BIT | MULTI_BYTE_BIT | address, 2, buf);
+  buf.order(ByteOrder.LITTLE_ENDIAN);
+  return buf.getShort(0);
+}
+
+public int red() {
+ return readWordRegister(LS_DATA_RED_1);
+}
+
+public int green() {
+ return readWordRegister(LS_DATA_GREEN_1);
+}
+
+public int blue() {
+ return readWordRegister(BDATA_REGISTER);
+}
+
+public int clear() {
+ return readWordRegister(CDATA_REGISTER);
+}
+
+public int proximity() {
+ return readWordRegister(PDATA_REGISTER);
+}
+}
+/*    protected final static int CMD = 0x80;
+protected final static int MULTI_BYTE_BIT = 0x20;
+protected final static int ENABLE_REGISTER  = 0x00;
+protected final static int ATIME_REGISTER   = 0x01;
+protected final static int PPULSE_REGISTER  = 0x0E;
+
+protected final static int ID_REGISTER     = 0x12;
+protected final static int CDATA_REGISTER  = 0x14;
+protected final static int RDATA_REGISTER  = 0x16;
+protected final static int GDATA_REGISTER  = 0x18;
+protected final static int BDATA_REGISTER  = 0x1A;
+protected final static int PDATA_REGISTER  = 0x1C;
+
+protected final static int PON   = 0b00000001;
+protected final static int AEN   = 0b00000010;
+protected final static int PEN   = 0b00000100;
+protected final static int WEN   = 0b00001000;
+protected final static int AIEN  = 0b00010000;
+protected final static int PIEN  = 0b00100000;
+
+private final double integrationTime = 10;
+
+
+private I2C sensor;
+
+private ByteBuffer buffy = ByteBuffer.allocate(8);
+
+public short red = 0, green = 0, blue = 0, prox = 0;
+
+public ColorSensor(I2C.Port port) {
+	buffy.order(ByteOrder.LITTLE_ENDIAN);
+    sensor = new I2C(port, 0x39); //0x39 is the address of the Vex ColorSensor V2
+    
+    sensor.write(CMD | 0x00, PON | AEN | PEN);
+    
+    sensor.write(CMD | 0x01, (int) (256-integrationTime/2.38)); //configures the integration time (time for updating color data)
+    sensor.write(CMD | 0x0E, 0b1111);
+    
+}
+
+public void read() {
+	buffy.clear();
+    sensor.read(CMD | MULTI_BYTE_BIT | RDATA_REGISTER, 8, buffy);
+    
+    red = buffy.getShort(0);
+    if(red < 0) { red += 0b10000000000000000; }
+    
+    green = buffy.getShort(2);
+    if(green < 0) { green += 0b10000000000000000; }
+    
+    blue = buffy.getShort(4); 
+    if(blue < 0) { blue += 0b10000000000000000; }
+    
+    prox = buffy.getShort(6); 
+    if(prox < 0) { prox += 0b10000000000000000; }
+    
+}
+
+public int status() {
+	buffy.clear();
+	sensor.read(CMD | 0x13, 1, buffy);
+	return buffy.get(0);
+}
+
+public void free() {
+	//sensor.free();
+}
+}
+*/
+
+/* package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.I2C;
+import java.nio.ByteBuffer;
+
+public class ColorSensor {
+    protected final static int COMMAND_REGISTER_BIT = 0x80;
+    I2C sensor;
+    public ColorSensor() {
+        sensor = new I2C(I2C.Port.kOnboard, 0x39);
+        sensor.write(COMMAND_REGISTER_BIT | 0x00, 0b00000011);
+    }
+
+    public void update(){
+        System.out.println(sensor.read(0x60, 7, ByteBuffer.allocate(7)));
+    }
+} */
---
+++ SensorTest/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,212 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Ultrasonic;
+//import edu.wpi.first.wpilibj.AnalogInput;
+
+/**
+ * This is a demo program showing the use of the RobotDrive class. The
+ * SampleRobot class is the base of a robot application that will automatically
+ * call your Autonomous and OperatorControl methods at the right time as
+ * controlled by the switches on the driver station or the field controls.
+ *
+ * <p>The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the SampleRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.properties file in the
+ * project.
+ *
+ * <p>WARNING: While it may look like a good choice to use for your code if
+ * you're inexperienced, don't. Unless you know what you are doing, complex code
+ * will be much more difficult under this system. Use TimedRobot or
+ * Command-Based instead if you're new.
+ */
+public class Robot extends SampleRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+
+  private final DifferentialDrive m_robotDrive
+      = new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
+  private final Joystick m_stick = new Joystick(0);
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  private Ultrasonic ultra;
+  //private AnalogInput exampleAnalog;
+  //private int bits;
+  private ColorSensor color_sensor;
+
+  private static final int ULTRASONIC_ECHO_PULSE_OUTPUT=1;
+  private static final int ULTRASONIC_TRIGGER_PULSE_INPUT=1;
+
+  public Robot() {
+    m_robotDrive.setExpiration(0.1);
+    ultra = new Ultrasonic(ULTRASONIC_ECHO_PULSE_OUTPUT, ULTRASONIC_TRIGGER_PULSE_INPUT); // creates the ultra object andassigns ultra to be an ultrasonic sensor which uses DigitalOutput 1 for 
+        // the echo pulse and DigitalInput 1 for the trigger pulse
+    //exampleAnalog = new AnalogInput(0);
+    color_sensor=new ColorSensor(0);
+  }
+
+  public boolean detectRed(){
+    if (color_sensor.red()>=150)
+      return true;
+    else
+      return false;
+  }
+
+  public boolean detectBlue(){
+    if (color_sensor.blue()>=110)
+      return true;
+    else
+      return false;
+  }
+
+  public boolean detectTape(){
+    if (color_sensor.alpha()<=0.3)
+      return true;
+    else
+      return false;
+  }
+  
+  //Analog
+  /* public void analogOversampleAverage(){
+    exampleAnalog.setOversampleBits(4);
+    bits = exampleAnalog.getOversampleBits();
+    exampleAnalog.setAverageBits(2);
+    bits = exampleAnalog.getAverageBits();
+  }
+  */
+
+  public void ultrasonicSample() {
+    double range = ultra.getRangeInches(); // reads the range on the ultrasonic sensor
+  }
+
+  @Override
+  public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto modes", m_chooser);
+    ultra.setAutomaticMode(true); // turns on automatic mode
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable
+   * chooser code works with the Java SmartDashboard. If you prefer the
+   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+   * getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to
+   * the if-else structure below with additional strings. If using the
+   * SendableChooser make sure to add them to the chooser code above as well.
+   *
+   * <p>If you wanted to run a similar autonomous mode with an TimedRobot
+   * you would write:
+   *
+   * <blockquote><pre>{@code
+   * Timer timer = new Timer();
+   *
+   * // This function is run once each time the robot enters autonomous mode
+   * public void autonomousInit() {
+   *     timer.reset();
+   *     timer.start();
+   * }
+   *
+   * // This function is called periodically during autonomous
+   * public void autonomousPeriodic() {
+   * // Drive for 2 seconds
+   *     if (timer.get() < 2.0) {
+   *         myRobot.drive(-0.5, 0.0); // drive forwards half speed
+   *     } else if (timer.get() < 5.0) {
+   *         myRobot.drive(-1.0, 0.0); // drive forwards full speed
+   *     } else {
+   *         myRobot.drive(0.0, 0.0); // stop robot
+   *     }
+   * }
+   * }</pre></blockquote>
+   */
+  @Override
+  public void autonomous() {
+    String autoSelected = m_chooser.getSelected();
+    // String autoSelected = SmartDashboard.getString("Auto Selector",
+    // defaultAuto);
+    System.out.println("Auto selected: " + autoSelected);
+
+    // MotorSafety improves safety when motors are updated in loops
+    // but is disabled here because motor updates are not looped in
+    // this autonomous mode.
+    m_robotDrive.setSafetyEnabled(false);
+
+    switch (autoSelected) {
+      case kCustomAuto:
+        // Spin at half speed for two seconds
+        m_robotDrive.arcadeDrive(0.0, 0.5);
+        Timer.delay(2.0);
+
+        // Stop robot
+        m_robotDrive.arcadeDrive(0.0, 0.0);
+        break;
+      case kDefaultAuto:
+      default:
+        // Drive forwards for two seconds
+        m_robotDrive.arcadeDrive(-0.5, 0.0);
+        Timer.delay(2.0);
+
+        // Stop robot
+        m_robotDrive.arcadeDrive(0.0, 0.0);
+        break;
+    }
+  }
+
+  /**
+   * Runs the motors with arcade steering.
+   *
+   * <p>If you wanted to run a similar teleoperated mode with an TimedRobot
+   * you would write:
+   *
+   * <blockquote><pre>{@code
+   * // This function is called periodically during operator control
+   * public void teleopPeriodic() {
+   *     myRobot.arcadeDrive(stick);
+   * }
+   * }</pre></blockquote>
+   */
+  @Override
+  public void operatorControl() {
+    m_robotDrive.setSafetyEnabled(true);
+    while (isOperatorControl() && isEnabled()) {
+      // Drive arcade style
+      m_robotDrive.arcadeDrive(-m_stick.getY(), m_stick.getX());
+
+      // The motors will be updated every 5ms
+      Timer.delay(0.005);
+    }
+  }
+
+  /**
+   * Runs during test mode.
+   */
+  @Override
+  public void test() {
+    //Moves/repositions the robot then puts a hatch on
+    if (!detectTape()){
+      m_Drivetrain.drive(0,0.2); //time this
+    }
+    else {
+      m_Drivetrain.drive(0);
+      m_hatch.extendHatch();
+    }
+  }
+}
